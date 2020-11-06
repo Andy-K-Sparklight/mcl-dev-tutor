411e66bd247d8264c2a15e8bc71ea236c17985c1
@s❤Rainbow <Twilight_Princess_Sparkle@outlook.com>
2020-11-07 07:36:41 +0800

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,20 +1,28 @@
-var AllPonyEmoji = {
-	"&ts-think": "https://www.picbed.cn/images/2020/11/06/ts_think.png"
-};
-window.$docsify.plugins.push(
-function (hook, vm){
-	hook.beforeEach(function (content){
-	var c = content;
-		for (var em of Object.entries(AllPonyEmoji.entries)){
-			c = replaceAll(c,em[0],"<img width='80' height='72' href='"+em[1]+"'></img>);
-		}
-	return c;
-	})
-})
-function replaceAll(origin,s,j){
-   if (s in origin.replace(s,j)){
-		return replaceAll(origin.replace(s,j),s,j)
-	}else{
-		return origin.replace(s,j)
-	}
+var AllPonyEmoji = [
+  {
+    key: "ts-think",
+    url: "https://www.picbed.cn/images/2020/11/06/ts_think.png",
+    width: 80,
+    height: 72,
+  },
+];
+window.$docsify.plugins.push(function (hook, vm) {
+  hook.beforeEach(function (content) {
+    var c = content;
+    for (var em of AllPonyEmoji) {
+      c = replaceAll(
+        c,
+        "&" + em.key,
+        `<img width='${em.width}' height='${em.height}' href='${em.url}'></img>`
+      );
+    }
+    return c;
+  });
+});
+function replaceAll(origin, s, j) {
+  if (s in origin.replace(s, j)) {
+    return replaceAll(origin.replace(s, j), s, j);
+  } else {
+    return origin.replace(s, j);
+  }
 }
